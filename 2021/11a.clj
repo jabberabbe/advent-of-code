588604ee241804a47ad58cc4389947a15985687d
Tito Sacchi <tito@tilde.team>
2021-12-12 20:14:27 +0100

    2021/11

---
+++ 2021/11a.clj
@@ -0,0 +1,42 @@
+(ns aoc.2021.11a)
+
+(def n 10)
+(def input (->> (slurp *in*)
+                (seq)
+                (filter (partial not= \newline))
+                (mapv #(Integer/parseInt (str %)))))
+(defn at [[x y]] (+ (* y n) x))
+(defn coord [c] [(rem c n) (quot c n)])
+(defn valid [[x y]] (and (< x n) (>= x 0) (< y n) (>= y 0)))
+(defn energy-increase [x]
+  (if (= x :flashed) :flashed (inc x)))
+(defn has-to-flash [x]
+  (if (= x :flashed) false (> x 9)))
+(defn single-step [xs]
+  (loop [energy  (mapv inc xs)
+         flashed 0]
+    (let
+      [flashing-elements   (keep-indexed #(when (has-to-flash %2) %1) energy)
+       surrounding         (vec (for [e     flashing-elements
+                                      dx    [-1 0 +1]
+                                      dy    [-1 0 +1]
+                                      :when (or (not= dx 0) (not= dy 0))
+                                      :let  [[x y] (coord e)
+                                             shifted [(+ x dx) (+ y dy)]]
+                                      :when (valid shifted)]
+                                  (at shifted)))
+       updated-flashing    (reduce #(assoc! %1 %2 :flashed)
+                                   (transient energy) flashing-elements)
+       updated-surrounding (persistent!
+                             (reduce #(assoc! %1 %2 (energy-increase (get %1 %2)))
+                                     updated-flashing surrounding))]
+      (if
+        (empty? flashing-elements)
+        [(mapv #(if (= % :flashed) 0 %) energy) flashed]
+        (recur updated-surrounding (+ flashed (count flashing-elements)))))))
+
+(println (->> [input 0]
+              (iterate (comp single-step first))
+              (take 101)
+              (map last)
+              (apply +)))
